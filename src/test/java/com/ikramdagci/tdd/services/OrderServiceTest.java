841b9cca5cd57797d7c9def87caed30c03c81810
İkram Dağcı <ikramdgc@gmail.com>
2023-08-04 17:01:44 +0300

    test(order) : added parameterized test

--- src/test/java/com/ikramdagci/tdd/services/OrderServiceTest.java
+++ src/test/java/com/ikramdagci/tdd/services/OrderServiceTest.java
@@ -2,19 +2,34 @@ package com.ikramdagci.tdd.services;
 
 import com.ikramdagci.tdd.dto.OrderDto;
 import com.ikramdagci.tdd.services.requests.CreateOrderRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class OrderServiceTest {
 
+    private OrderService orderService;
+    @BeforeEach
+    public void beforeEach(){
+        orderService = new OrderService();
+    }
+    public static Stream<Arguments> order_requests() {
+        return Stream.of(
+                Arguments.of("code1",5,BigDecimal.valueOf(12.3),BigDecimal.valueOf(61.5)),
+                Arguments.of("code2",10,BigDecimal.valueOf(15),BigDecimal.valueOf(150))
+        );
+    }
+
     @Test
     public void itShouldCeateOrderWith5Items(){
         //given
-        OrderService service = new OrderService();
         CreateOrderRequest request = CreateOrderRequest.builder()
                 .productCode("code1")
                 .amount(5)
@@ -22,16 +37,15 @@ public class OrderServiceTest {
                 .build();
 
         //when
-        OrderDto orderDto = service.createOrder(request);
+        OrderDto orderDto = orderService.createOrder(request);
         //then
         then(orderDto).isNotNull();
         then(orderDto.getTotalPrice()).isEqualTo(BigDecimal.valueOf(61.5));
     }
 
     @Test
-    public void itShouldCreateOrderWith10Items(){
+    public void itShouldCeateOrderWith10Items(){
         //given
-        OrderService service = new OrderService();
         CreateOrderRequest request = CreateOrderRequest.builder()
                 .productCode("code1")
                 .amount(10)
@@ -39,12 +53,27 @@ public class OrderServiceTest {
                 .build();
 
         //when
-        OrderDto orderDto = service.createOrder(request);
+        OrderDto orderDto = orderService.createOrder(request);
         //then
         then(orderDto).isNotNull();
         then(orderDto.getTotalPrice()).isEqualTo(BigDecimal.valueOf(150D));
     }
 
+    @ParameterizedTest
+    @MethodSource("order_requests")
+    public void itShouldCreateOrders(String productCode,Integer amount,BigDecimal unitPrice,BigDecimal expectedTotalPrice) {
+        //given
+        final CreateOrderRequest request = CreateOrderRequest.builder()
+                .productCode(productCode)
+                .amount(amount)
+                .unitPrice(unitPrice)
+                .build();
+        //when
+        final OrderDto order = orderService.createOrder(request);
 
+        //then
+        then(order.getTotalPrice()).isEqualTo(expectedTotalPrice);
+
+    }
 
 }
